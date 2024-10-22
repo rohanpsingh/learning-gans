fe0d03d5510fa3a05cec20845bd4ab9b4cbcd690
Rohan P. Singh <rohan565singh@gmail.com>
2024-10-22 14:49:50 +0900

    update script for VAE inference

--- predict_vae.py
+++ predict_vae.py
@@ -12,6 +12,35 @@ import matplotlib.pyplot as plt
 
 from net import *
 
+def plot_latent_space(net, device, scale=1.0, n=25, digit_size=28, figsize=15):
+    # display a n*n 2D manifold of digits
+    figure = np.zeros((digit_size * n, digit_size * n))
+
+    # construct a grid 
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = torch.tensor([[xi, yi]], dtype=torch.float).to(device)
+            x_decoded = net.decode(z_sample)
+            digit = x_decoded[0].detach().cpu().reshape(digit_size, digit_size)
+            figure[i * digit_size : (i + 1) * digit_size, j * digit_size : (j + 1) * digit_size,] = digit
+
+    plt.figure(figsize=(figsize, figsize))
+    plt.title('VAE Latent Space Visualization', fontsize=28)
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x, fontsize=18)
+    plt.yticks(pixel_range, sample_range_y, fontsize=18)
+    plt.xlabel("mean, z [0]", fontsize=28)
+    plt.ylabel("var, z [1]", fontsize=28)
+    plt.imshow(figure, cmap="Greys_r")
+    plt.show()
+
 def generate_digit(net, mean, var, device):
     z_sample = torch.tensor([[mean, var]], dtype=torch.float).to(device)
     x_decoded = net.decode(z_sample)
@@ -52,9 +81,12 @@ def main():
     net = initialize_net(args.model).to(device)
 
     # generate new images
-    for _ in range(10):
+    for _ in range(5):
         z = np.random.uniform(-2, 2, 2)
         generate_digit(net, *z, device)
+
+    # plot the latent space
+    plot_latent_space(net, device)
 
 if __name__=="__main__":
     main()
